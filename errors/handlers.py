015c668463963dea6344f5e74222cae38220b097
Jvitor <jvitorsaugusto@outlook.com>
2026-06-24 21:00:25 -0300

    feat: implementada nova exceção personalizada para status invalido em OPS

--- errors/handlers.py
+++ errors/handlers.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from errors.errors_domain.production_order_errors import ProductionOrderInvalidStatusException
 from errors.exceptions import NotFoundException
 
 
@@ -8,3 +9,7 @@ def register_exception_handlers(app: FastAPI) -> None:
     @app.exception_handler(NotFoundException)
     async def not_found_handler(request: Request, exc: NotFoundException):
         return JSONResponse(status_code=404, content={"detail": f"{exc.resource_name} não encontrado(a)."})
+    
+    @app.exception_handler(ProductionOrderInvalidStatusException)
+    async def op_invalid_status(request: Request, exc: ProductionOrderInvalidStatusException):
+        return JSONResponse(status_code=400, content={"detail": f"O status finalizado não pode ser atribuido a uma Ordem de Produção pendente - OP: {exc.op_code}"})
