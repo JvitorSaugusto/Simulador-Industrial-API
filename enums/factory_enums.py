e9a61ef800d518bfc76b6d2f860c5fd310439499
Jvitor <jvitorsaugusto@outlook.com>
2026-06-26 12:05:12 -0300

    feat: Novos enums de DownTime adicionados e adição de comentarios para orientação

--- enums/factory_enums.py
+++ enums/factory_enums.py
@@ -1,14 +1,26 @@
 from enum import Enum
 
-
-class LineStatusEnum(str, Enum):
+# LINHAS DE PRODUÇÃO
+class MachineStatusEnum(str, Enum):
     IDLE = "idle"
     STOP = "stop"
     MAINTENANCE = "maintenance"
     PRODUCTION = "production"
-    
+
+# OPS
 class ProductionOrderEnum(str, Enum):
     STOP = "stop"
     FINISHED = "finished"
     PENDING = "pending"
     PRODUCTION = "production"
+    
+# EVENTOS DE PARADA
+class DownTimeEventTypeEnum(str, Enum):
+    FAILURE = "failure"
+    PREVENTIVE = "preventive_maintenance"
+    INSPECTION = "inspection"
+    
+class DownTimeSeverityEnum(str, Enum):
+    LOW = "low"
+    MEDIUM = "mid"
+    HIGH = "high"
