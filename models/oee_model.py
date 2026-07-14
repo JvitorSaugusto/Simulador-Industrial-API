dfa073362ccbecae81c043abc94386e634c02cee
Jvitor <jvitorsaugusto@outlook.com>
2026-07-14 11:11:54 -0300

    refactor: removendo relacionamento com linhas de producao

--- models/oee_model.py
+++ models/oee_model.py
@@ -5,7 +5,6 @@ from sqlalchemy import DateTime, ForeignKey, Integer, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
-from models.production_line_model import ProductionLineModel
 from models.production_order_model import ProductionOrderModel
 
 
@@ -15,8 +14,6 @@ class OeeRecordModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     production_order_id: Mapped[int] = mapped_column(ForeignKey("production_orders.id", ondelete="CASCADE"))
     production_order: Mapped[ProductionOrderModel] = relationship(back_populates="oees_records") 
-    production_line_id: Mapped[int] = mapped_column(ForeignKey("production_lines.id", ondelete="CASCADE"))
-    production_line: Mapped[ProductionLineModel] = relationship(back_populates="oees_records")
     availability: Mapped[Decimal] = mapped_column(Numeric(5, 2))
     performance: Mapped[Decimal] = mapped_column(Numeric(5, 2))
     quality: Mapped[Decimal] = mapped_column(Numeric(5, 2))
